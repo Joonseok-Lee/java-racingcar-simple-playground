49faa1aceb68e2727f602f5a7282327b55357091
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-13 22:45:01 +0900

    feat: 게임 시작, turn에 할당된 횟수만큼 반복하며 차량을 일제히 움직이고, 가장 높은 거리를 기반으로 우승자를 배열로 반환하게끔 작성

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -17,5 +18,54 @@ public class RacingGame {
         for (int i = 0; i < count; i++) {
             this.carSet.add(new MovableCar(i + "번 차"));
         }
+    }
+
+    public void game() {
+        gameStart();
+    }
+
+    private void gameStart() {
+        RaceStart();
+
+        int highestDistance = getHighestDistance();
+        List<MovableCar> winnerList = getWinnerList(highestDistance);
+
+    }
+
+    private List<MovableCar> getWinnerList(int highestDistance) {
+        return carSet.parallelStream()
+                .filter(car ->
+                        car.getDistance() == highestDistance)
+                .toList();
+    }
+
+    private void RaceStart() {
+        for(int i = 0; i < turn; i++)
+            moveAllCar();
+    }
+
+    private void moveAllCar() {
+        carSet.stream()
+                .parallel()
+                .forEach(MovableCar::move);
+    }
+
+    private int getHighestDistance() {
+        int highestDistance = 0;
+
+        for(MovableCar car : carSet) {
+            highestDistance = compareDistance(car, highestDistance);
+        }
+
+        return highestDistance;
+    }
+
+    private int compareDistance(MovableCar car, int highestDistance) {
+
+        if(car.getDistance() >= highestDistance) {
+            highestDistance = car.getDistance();
+        }
+
+        return highestDistance;
     }
 }
