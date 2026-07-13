eae9180a206310af56d42b418819e4f33f101f0d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-13 22:03:10 +0900

    feat: 이름을 가진 움직이는 자동차 객체 작성

---
+++ src/main/java/MovableCar.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class MovableCar {
+
+    private String name;
+
+    public MovableCar(String name) {
+        this.name = name;
+    }
+
+    public String getName() { return name; }
+}
