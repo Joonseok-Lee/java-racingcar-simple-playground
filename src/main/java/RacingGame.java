c45c8e35add94c740f26499caf3dc1cdf2d3bf92
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-14 14:34:05 +0900

    feat: 3단게 게임 진행 방식처럼 로직의 순서를 수정하고, 차량이 기존 i번 차라는 이름을 갖는 로직을 1회의 문자열 입력으로 n대의 차량 이름을 지정할 수 있게 수정

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -11,13 +11,15 @@ public class RacingGame {
     public RacingGame(Set<MovableCar> carSet) {
         int count = scanner.nextInt();
         carSetInit(count);
+        System.out.println("시도할 회수는 몇회인가요?");
         this.turn = scanner.nextInt();
     }
 
     private void carSetInit(int count) {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+        String[] names = scanner.nextLine().split(",");
         for (int i = 0; i < count; i++) {
-            String name = scanner.nextLine();
-            this.carSet.add(new MovableCar(name));
+            this.carSet.add(new MovableCar(names[i]));
         }
     }
 
