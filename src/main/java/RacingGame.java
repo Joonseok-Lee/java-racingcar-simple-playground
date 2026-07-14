4ec8939b49fb635088fe3bf4610d692d8b4f9420
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-14 20:28:14 +0900

    feat: 게임의 매 턴마다, 각 차량의 진행 상황을 출력하는 printTurnResult 메소드를 RaceStart 메소드 내에서 실행

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -40,8 +40,10 @@ public class RacingGame {
 
     private void RaceStart() {
         System.out.println("실행 결과");
-        for(int i = 0; i < turn; i++)
+        for(int i = 0; i < turn; i++) {
             moveAllCar();
+            printTurnResult();
+        }
     }
 
     private void printTurnResult() {
