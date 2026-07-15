3844efc95b360a9ada9014d4f54b53cdcb8694d9
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-15 10:12:56 +0900

    refactor: main 메소드에서 InputView의 정적 메소드를 사용하여 차량 이름과, 몇 회수를 진행할 것인가에 대한 턴 수를 변수로 할당 후, RacingGame에 전달하도록 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import view.InputView;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        RacingGame racingGame = new RacingGame(new InputView());
+        final List<String> carNames = InputView.initCarName();
+        final int turnCount = InputView.initTurnCount();
+
+        RacingGame racingGame = new RacingGame(carNames, turnCount);
         racingGame.start();
     }
 }
