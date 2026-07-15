5c64aaaad96a9dcd5fbc2af50886835063a3ceab
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-15 14:23:04 +0900

    fix: 생성자 매개변수에 List<String> -> Set<MovableCar>로 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
+import domain.MovableCar;
 import domain.RacingGame;
+import domain.rand.MovableImpl;
 import view.InputView;
 import view.ResultView;
 
-import java.util.List;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        final List<String> carNames = InputView.initCarName();
+        final Set<MovableCar> cars = InputView.initCars();
         final int turnCount = InputView.initTurnCount();
 
-        RacingGame racingGame = new RacingGame(carNames, turnCount);
+        RacingGame racingGame = new RacingGame(cars, turnCount, new MovableImpl());
         racingGame.start();
 
         ResultView.printResult(racingGame.getWinnerNames());
