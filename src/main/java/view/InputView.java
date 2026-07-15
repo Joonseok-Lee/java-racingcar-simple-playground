c01ff13d43842912a89a078483cd589c994b2f61
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-15 22:10:17 +0900

    feat: InputView에 잘못된 입력값을 입력한 경우, 예외를 반환하도록 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,6 +3,7 @@ package view;
 import domain.MovableCar;
 
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -10,7 +11,21 @@ public class InputView {
 
     public static Set<MovableCar> initCars() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] names = new Scanner(System.in).nextLine().split(",");
+
+        String input;
+
+        while(true) {
+            input = new Scanner(System.in).nextLine();
+            try {
+                if (input.contains(" "))
+                    throw new IllegalArgumentException();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("입력값에는 공백이 포함될 수 없습니다.");
+            }
+        }
+
+        String[] names = input.split(",");
 
         Set<MovableCar> cars = new HashSet<>();
 
@@ -23,6 +38,19 @@ public class InputView {
 
     public static int initTurnCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return new Scanner(System.in).nextInt();
+
+        int turnCount;
+
+        while(true) {
+            try {
+                turnCount = new Scanner(System.in).nextInt();
+                if (turnCount < 0)
+                    throw new InputMismatchException();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("입력값은 음이 아닌 정수만 가능합니다.");
+            }
+        }
+        return turnCount;
     }
 }
