fa82ab002741dcef4309e6a7cd1145a1eda8fa3b
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-15 10:10:10 +0900

    fix: 레퍼런스 객체 생성을 하지 않아도, 정적 메소드로 해당 메소드를 호출할 수 있게 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private Scanner scanner = new Scanner(System.in);
-
-    public List<String> initPlayerName() {
+    public static List<String> initPlayerName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String[] names = scanner.nextLine().split(",");
+        String[] names = new Scanner(System.in).nextLine().split(",");
         return Arrays.asList(names);
     }
 
-    public int initTurnCount() {
+    public static int initTurnCount() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return scanner.nextInt();
+        return new Scanner(System.in).nextInt();
     }
 }
