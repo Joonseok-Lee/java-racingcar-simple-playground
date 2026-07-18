a742885a8cb124cd5b9bb9f38c6425f0a65d7742
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-18 22:05:40 +0900

    fix: psvm에 게임이 흘러가는 순서를 적어놓았던 것을 RacingGameApplication으로 이동, psvm에는 게임을 "실행"한다는 책임 1개만 부여

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,6 @@
-import domain.MovableCar;
-import domain.RacingGame;
-import domain.rand.MovableImpl;
-import view.InputView;
-import view.ResultView;
-
-import java.util.Set;
-
 public class Main {
 
     public static void main(String[] args) {
-        final Set<MovableCar> cars = InputView.initCars();
-        final int turnCount = InputView.initTurnCount();
-
-        RacingGame racingGame = new RacingGame(cars, turnCount, new MovableImpl());
-        racingGame.start();
-
-        ResultView.printFinalResult(racingGame.getWinnerNames());
+        RacingGameApplication.run();
     }
 }
