d1e243cd7f92f47b4bec9e4522e5d867f90a0b7a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-18 22:00:50 +0900

    fix: "실행 결과"를 출력하는 책임을 ResultView로 이동, 이동함에 따라 출력하는 메소드를 턴 결과 출력에서 게임 내용 출력으로 이름 변경, 턴 결과 출력 메소드는 private 메소드로 수정

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,13 +1,25 @@
 package view;
 
 import domain.MovableCar;
+import domain.RacingGame;
 
 import java.util.List;
 import java.util.Set;
 
 public class ResultView {
 
-    public static void printTurnResult(Set<MovableCar> cars) {
+    public static void printInGameResult(RacingGame racingGame) {
+        Set<MovableCar> cars = racingGame.getCarSet();
+        System.out.println("실행 결과");
+
+        for(int i = 0; i <= racingGame.getTurn(); i++) {
+            racingGame.start();
+            printTurnResult(cars);
+
+        }
+    }
+
+    private static void printTurnResult(Set<MovableCar> cars) {
         for(MovableCar car : cars) {
             System.out.println(car.getName() + " : " + car.getDistanceToString());
         }
