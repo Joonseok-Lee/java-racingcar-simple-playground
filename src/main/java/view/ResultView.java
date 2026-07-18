2c515e3061f08aff23a61ee467c48dfd1c57e22c
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-18 21:48:25 +0900

    feat: 매 턴 종료 시, 턴의 결과를 출력하는 printTurnResult 메소드를 추가, 그리고 해당 메소드를 추가함에 따라 printResult -> printFinalResult로 이름 수정

--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,10 +1,17 @@
 package view;
 
+import domain.MovableCar;
+
 import java.util.List;
+import java.util.Set;
 
 public class ResultView {
 
-    public static void printResult(List<String> winnerNames) {
+    public static void printTurnResult(Set<MovableCar> cars) {
+
+    }
+
+    public static void printFinalResult(List<String> winnerNames) {
         System.out.println(String.join(", ", winnerNames) + "가 최종 우승했습니다.");
     }
 }
