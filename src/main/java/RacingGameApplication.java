34aa6bf183897218c74c56d06e5612b3b9ddd138
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-07-22 11:27:24 +0900

    fix: 누락된 THRESHOLD 상수 관리 추가

--- src/main/java/RacingGameApplication.java
+++ src/main/java/RacingGameApplication.java
@@ -5,12 +5,13 @@ import view.InputView;
 
 public class RacingGameApplication {
 
-    private static final int MAX = 9;
     private static final int MIN = 0;
+    private static final int MAX = 9;
+    private static final int THRESHOLD = 4;
 
     public static void run() {
         InputView inputView = new InputView(System.in);
-        Move move = new RandomMove(MIN, MAX, 4);
+        Move move = new RandomMove(MIN, MAX, THRESHOLD);
 
         RacingGameController racingGameController = new RacingGameController(inputView, move);
         racingGameController.gameStart();
